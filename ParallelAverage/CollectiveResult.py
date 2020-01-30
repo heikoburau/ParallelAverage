62594c7050c4071faeb85f515be35a657c01117d
Heiko Burau <burau@pks.mpg.de>
2020-01-30 14:54:39 +0100

    add 'replace_output'

--- ParallelAverage/CollectiveResult.py
+++ ParallelAverage/CollectiveResult.py
@@ -1,4 +1,4 @@
-from .json_numpy import NumpyDecoder
+from .json_numpy import NumpyEncoder, NumpyDecoder
 import json
 import pickle
 
@@ -38,19 +38,26 @@ class CollectiveResult:
             run_id = repr(run_id)
 
         file_id = self.raw_results_map[run_id]
+        file_path = self.job_path / "data_output" / f"{file_id}_raw_results.{self.encoding}"
+
         if self.encoding == "json":
-            with open(self.job_path / "data_output" / f"{file_id}_raw_results.json") as f:
+            with open(file_path) as f:
                 return json.load(f, cls=NumpyDecoder)[run_id]
         elif self.encoding == "pickle":
-            result_path = self.job_path / "data_output" / f"{file_id}_raw_results.pickle"
-            if result_path.stat().st_size > 0:
-                with open(result_path, 'rb') as f:
+            if file_path.stat().st_size > 0:
+                with open(file_path, 'rb') as f:
                     return pickle.load(f)[run_id]
             else:
                 return None
 
     def __len__(self):
         return len(self.run_ids)
+
+    def __repr__(self):
+        return repr(dict(self.items()))
+
+    def __str__(self):
+        return str(dict(self.items()))
 
     def keys(self):
         return self.run_ids
@@ -62,3 +69,41 @@ class CollectiveResult:
     def items(self):
         for run_id in self:
             yield run_id, self[run_id]
+
+    def replace_output(self, new_dict, new_encoding=None):
+        new_encoding = new_encoding or self.encoding
+
+        for run_id in new_dict:
+            if not isinstance(run_id, str):
+                run_id = repr(run_id)
+
+            file_id = self.raw_results_map[run_id]
+            file_path = self.job_path / "data_output" / f"{file_id}_raw_results.{self.encoding}"
+
+            if self.encoding == "json":
+                with open(file_path) as f:
+                    raw_results = json.load(f, cls=NumpyDecoder)
+            elif self.encoding == "pickle":
+                if file_path.stat().st_size > 0:
+                    with open(file_path, 'rb') as f:
+                        raw_results = pickle.load(f)
+                else:
+                    raw_results = {}
+
+            raw_results[run_id] = new_dict[eval(run_id)]
+
+            file_path = self.job_path / "data_output" / f"{file_id}_raw_results.{new_encoding}"
+
+            if new_encoding == "json":
+                with open(file_path, "w") as f:
+                    json.dump(raw_results, f, cls=NumpyEncoder, indent=2)
+            elif new_encoding == "pickle":
+                with open(file_path, "wb") as f:
+                    pickle.dump(raw_results, f)
+
+        if new_encoding != self.encoding:
+            for file_id in self.raw_results_map.values():
+                file_path = self.job_path / "data_output" / f"{file_id}_raw_results.{self.encoding}"
+                file_path.unlink()
+
+        self.encoding = new_encoding
